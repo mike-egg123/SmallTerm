ab66905eb027d5a0b7006470f2dbeb59b0614062
slamdunk77 <62142444+slamdunk77@users.noreply.github.com>
2020-08-10 16:45:52 +0800

    Update App.vue

--- diamond/src/App.vue
+++ diamond/src/App.vue
@@ -5,9 +5,20 @@
 </template>
 
 <script>
-
+import DiamondHeader from './components/DiamondHeader'
+import { Message } from 'element-ui'
 export default {
   name: 'App',
+  methods:{
+    handleClick(){
+      Message('弹出成功',{
+        showClose: true,
+        type: 'success',
+        duration: 1000
+      })
+
+    }
+  },
   components: {
   }
 }
