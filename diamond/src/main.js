96061a17a241ee921db69cb88f863a7630ad812a
slamdunk77 <62142444+slamdunk77@users.noreply.github.com>
2020-08-10 16:43:59 +0800

    Update main.js

--- diamond/src/main.js
+++ diamond/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import {Button} from 'element-ui'
+import {Link} from 'element-ui'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+Vue.component(Button.name,Button);
+Vue.component(Link.name,Link)
 
 new Vue({
+  el: '#app',
   render: h => h(App),
   router
 }).$mount('#app')
